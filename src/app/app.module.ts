1a32527de75d870f3e240e5f995b89dc1a3c5139
DhouhaOun <dhouha.ouni93@gmail.com>
2020-11-13 13:46:56 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BilletComponantComponent } from './home/shared/comps/billet-componant/billet-componant.component';
+import { TicketComponent } from './home/shared/comps/ticket/ticket.component';
+import { TicketListComponent } from './home/shared/comps/ticket-list/ticket-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BilletComponantComponent,
+    TicketComponent,
+    TicketListComponent
   ],
   imports: [
     BrowserModule
